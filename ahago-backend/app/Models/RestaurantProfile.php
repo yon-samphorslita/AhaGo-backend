24011d035fb094923f56f0b54e60c1cfcf3c4908
yonsamphorslita <yon.samphorslita@gmail.com>
2025-06-26 21:20:14 +0700

    fixed error in restprofile

--- ahago-backend/app/Models/RestaurantProfile.php
+++ ahago-backend/app/Models/RestaurantProfile.php
@@ -30,8 +30,4 @@ class RestaurantProfile extends Model
         return $this->hasMany(Category::class, 'restaurant_id');
     }
 
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
 }
